e82890b0dec778aa71f01cb06ca02c3264760965
Kristinevalm <kristinevalm@gmail.com>
2022-01-31 20:30:08 +0200

    fix image

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,6 @@
 const myButton = document.querySelector("button");
 const myImage = document.querySelector("img");
 myButton.addEventListener("click", () => {
-console.log("Click");
+console.log("click");
 myImage.src = "images/pic5.jpg";
 });
