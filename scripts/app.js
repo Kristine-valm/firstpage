37c2ee8dc3309e090e04f39ca9501cc939658010
Kristinevalm <kristinevalm@gmail.com>
2022-02-07 18:01:02 +0200

    add gallery

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,13 @@
 
 const myButton = document.querySelector("button");
 const myImage = document.querySelector("img");
+const images = ["pic1", "pic2", "pic3", "pic4", "pic5"];
+
 myButton.addEventListener("click", () => {
 console.log("click");
-myImage.src = "images/pic5.jpg";
+let randomIndex = Math.floor(Math.random() * images.length);
+console.log(randomIndex);
+let randomImagePath = "images/" + images[randomIndex] + ".jpg";
+console.log(randomImagePath);
+myImage.src = randomImagePath;
 });
